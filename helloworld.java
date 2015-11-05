c3cff60329011aa3857e1776c0972076e76a211d
web8557 <web8557@yahoo.com>
2015-11-05 09:45:59 -0800

    defects #1: change msg to T-Mobile

--- helloworld.java
+++ helloworld.java
@@ -4,6 +4,6 @@
  */
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); //Display the string.
+        System.out.println("Hello T-Mobile!"); //Display the string.
     }
 }
