970303ec08eb589f2a17ea4c5980cceacd5dd190
Bill Bennett <web@hpe.com>
2015-11-17 18:36:16 -0500

    Defect #2: trigger a build after enabling debug ALM logging

--- helloworld.java
+++ helloworld.java
@@ -11,6 +11,6 @@ class HelloWorld {
         System.out.println("Hello ALI");
         System.out.println("Add Release info");
         System.out.println("Link to a defect");
-        System.out.println("CodeChange test 5");
+        System.out.println("CodeChange test 6");
     }
 }
