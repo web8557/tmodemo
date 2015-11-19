e91242f39be1e0c1ab782df692eddb1a0f3e829d
Bill Bennett <web@hpe.com>
2015-11-19 11:42:08 -0500

    Defect #2: do code changes propagate with defects?

--- helloworld.java
+++ helloworld.java
@@ -16,5 +16,6 @@ class HelloWorld {
         System.out.println("Try another build");
         System.out.println("Add SCM to build config");
         System.out.println("Delete and re-add ALM build config3");
+        System.out.println("Now link to defect #2");
     }
 }
