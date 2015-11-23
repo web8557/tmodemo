09bd4260053a7c922a344b478efc60da36f04b8e
Bill Bennett <web@hpe.com>
2015-11-23 14:31:35 -0500

    Requirement #2: Run test set from Jenkins

--- helloworld.java
+++ helloworld.java
@@ -9,6 +9,7 @@ class HelloWorld {
 	//another change, be sure to get the tag correct!
     //Finally!  Eclipse "sees" tmodemo on github
         System.out.println("Hello ALI");
+        System.out.println("Run ALM test set");
         System.out.println("Add Release info");
         System.out.println("Link to a defect");
         System.out.println("CodeChange test 7");
